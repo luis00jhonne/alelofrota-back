5e122669d413f130b309c863b9b6bccdd278cb0b
Luis Jhonne Carvalhal de Melo <luis.jhonne@gmail.com>
2021-01-19 10:10:39 -0300

    M - Status correto na exception

--- src/main/java/br/com/alelo/frota/exceptions/VehicleNotFoundException.java
+++ src/main/java/br/com/alelo/frota/exceptions/VehicleNotFoundException.java
@@ -1,5 +1,9 @@
 package br.com.alelo.frota.exceptions;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
 public class VehicleNotFoundException extends Exception {
 
     private static final long serialVersionUID = -2586209354700102349L;
